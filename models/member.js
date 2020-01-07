be061dce9814fdfdc4ca4c91fb2f85871771bbb5
YiChing316 <44517980+YiChing316@users.noreply.github.com>
2020-01-07 17:25:36 +0800

    增加註冊post的function

--- models/member.js
+++ models/member.js
@@ -1,1 +1,41 @@
-var pool = require('./connectMysql');
+var pool = require('./connectMysql'),
+    crypto = require('crypto');//加密
+
+module.exports = {
+
+    //加密
+    hash: function(str){
+        return crypto.createHmac('md5', str).update(str).digest('hex');
+    },
+
+    //登入
+    login: function(){
+        
+    },
+
+    //註冊post
+    register: function(member_name,member_city,member_school,member_account,member_password,cb){
+        pool.getConnection(function(err,connection){
+            if(err) throw err;
+
+            var sql = {
+                member_name: member_name,
+                member_city: member_city,
+                member_school: member_school,
+                member_account: member_account,
+                member_password: member_password,
+                member_identity: '教師'
+            };
+
+            console.log(sql);
+
+            connection.query('INSERT INTO `member` SET ?',sql,function(err,results){
+                if(err) throw err;
+                cb(results);
+
+                connection.release();
+            });
+        })
+    }
+
+}
