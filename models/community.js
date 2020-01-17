0869704329110ad52487da93fa42af57b97b5a4d
YiChing316 <44517980+YiChing316@users.noreply.github.com>
2020-01-17 17:32:08 +0800

    Create community.js
    1.新增create社群並同時將創建人加入社群成員名單
    2.顯示現有的社群function尚未完成

---
+++ models/community.js
@@ -0,0 +1,55 @@
+var pool = require('./connectMysql');
+
+module.exports = {
+    create: function(community_name,community_key,member_id,member_name,cb){
+        pool.getConnection(function(err,connection){
+            if(err) throw err;
+
+            var sql = {
+                community_name: community_name,
+                community_key: community_key,
+                member_id_member: member_id,
+                member_name: member_name
+            }
+
+            connection.query('INSERT INTO `community` SET ?',sql,function(err,results){
+                if(err) throw err;
+                cb(results);
+                connection.release();
+            })
+        })
+    },
+
+    addCommunityMember: function(community_id,member_id,member_name,cb){
+        pool.getConnection(function(err,connection){
+            if(err) throw err;
+
+            var sql = {
+                community_id_community: community_id,
+                member_id_member: member_id,
+                member_name: member_name
+            }
+
+            connection.query('INSERT INTO `community_member` SET ?',sql,function(err,results){
+                if(err) throw err;
+                cb(results);
+                connection.release();
+            })
+        })
+    },
+
+    showAllCommunity: function(cb){
+        pool.getConnection(function(err,connection){
+            if(err) throw err;
+            connection.query('SELECT * FROM `community`',function(err,results){
+                if(err) throw err;
+                cb(results);
+                connection.release;
+            })
+        })
+    },
+
+    showMemberCommunity: function(member_id,cb){
+
+    }
+}
