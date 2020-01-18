8901124348181d904753c5cc09fbb57b1218406b
YiChing316 <44517980+YiChing316@users.noreply.github.com>
2020-01-18 16:53:33 +0800

    Update dashboard.js
    增加每個社群要輸入密碼的tr視覺效果

--- public/javascripts/dashboard.js
+++ public/javascripts/dashboard.js
@@ -29,6 +29,7 @@ function createCommunity(){
 function showAllCommunity(){
     for(var i = 0; i<allCommunityData.length;i++){
         var community = allCommunityData[i];
+        var community_id = community.community_id;
         var community_name = community.community_name;
         //sql出來的時間為ISO Date ex:"2015-03-25T12:00:00Z"，new Date後會變成 Wed Mar 25 2015 08:00:00 GMT+0800
         var community_createtime = new Date(community.community_createtime);
@@ -39,11 +40,30 @@ function showAllCommunity(){
         var allCommunity_div = ["<tr>"+
                                     "<td>"+ community_name +"</td>"+
                                     "<td>"+ year+"/"+month+"/"+date +"</td>"+
-                                    "<td><input type='button' value='加入社群'></td>"+
-                                "</tr>"];
+                                    "<td><input type='button' id='enterCommunity"+community_id+"' value='加入社群' onclick='showEntertr("+community_id+")'></td>"+
+                                "</tr>"+
+                                "<tr id='entertr"+community_id+"' style='display:none'>"+
+                                    "<td>密碼:<input type='text' id='community_key_"+community_id+"'></td>"+
+                                    "<td><input type='button' value='送出'><input type='button' value='取消加入' onclick='hideEntertr("+community_id+")'></td>"+
+                                    "<td><p>沒有密碼嗎?</p><a href=''>提出申請</a></td>"+
+                                "</tr>"
+                                ];
 
         $("#allCommunityTable tbody").append(allCommunity_div);
     }
+}
+
+//根據點選'加入社群'按鈕所對應的id去顯示該id的'entertr'
+function showEntertr(id){
+    $("#entertr"+id).show();//這邊tr的style會移除dispaly:none的屬性
+    //同時此按鈕消失
+    $("#enterCommunity"+id).hide();
+}
+//上方程式變成隱藏
+function hideEntertr(id){
+    $("#entertr"+id).hide();
+    //'加入社群'的按鈕會顯示
+    $("#enterCommunity"+id).show();
 }
 
 
