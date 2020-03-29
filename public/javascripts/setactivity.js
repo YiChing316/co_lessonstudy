1cace6f80ac8f6dcd9de07348e2dd55e67a05da0
YiChing316 <44517980+YiChing316@users.noreply.github.com>
2020-03-29 17:46:31 +0800

    將活動table改為bootstraptable
    還沒完成

--- public/javascripts/setactivity.js
+++ public/javascripts/setactivity.js
@@ -3,8 +3,10 @@ var common_option = ["引起動機","發展活動","綜合活動","課堂結束"
 var assessment_option = ["紙筆測驗","口頭評量","實作能力評量","作品評量","態度評量","課室教學記錄","學習反思札記","同儕互評","學習歷程檔案評量","評量指標"];
 
 $(function(){
+    activityProcessTable();
+
     sorttableTbody('.activityTbody');
-    deletetableTr('.activityTbody');
+    // deletetableTr('.activitytable tbody');
     processselect_Set();
     assessment_Set();
     processscaffold_Add();
@@ -16,8 +18,26 @@ $(function(){
         var id = button.data('targettr');
         var modal = $(this);
         modal.find('#targetid').text(id);
+        // assessmentsummernote();
     })
 })
+
+function activityProcessTable(){
+    $activitytable = $("#activity_1Table");
+    $activitytable.bootstrapTable({
+        columns:[
+            {title:"#",field:"tr_num",width:10},
+            {title:"學習目標",field:"activity_process_learningtarget",width:90},
+            {title:"活動流程",field:"activity_process_content",width:400},
+            {title:"時間",field:"activity_process_time",width:60},
+            {title:"評量方式",field:"activity_process_assessment",width:150},
+            {title:"備註",field:"activity_process_remark",width:90},
+            {formatter:"processBtnFormatter",events:"processBtnEvents"}
+        ],
+        theadClasses:'thead-light',
+        classes:'table table-bordered mt-3'
+    });
+}
 
 function processselect_Set(){
     $.each(explore_option, function(i, val) {
@@ -63,7 +83,8 @@ function processscaffold_Add(){
 function assessmentscaffold_Add(){
     $("#assessment_sel").change(function(){
         var value = $("#assessment_sel :selected").val();
-        $('#assessmentsummernote').summernote('code', value);
+        $("#assessmentsummernote").summernote('pasteHTML', value);
+        // $('#assessmentsummernote').summernote('code', value);
     })
 }
 
@@ -73,8 +94,9 @@ function addactivityTr(){
     var processtime = $("#processtime").val();
     var processremark = $("#processremark").val();
 
-    var num = $("#activity_1Tbody").find("tr").last().children("th").text();
-    num++
+    //數字抓取不正確
+    var num = $(".activitytable tbody").find("tr").last().children("td").text();
+    num++;
 
     if(processcontent == "" || processtime == ""){
         $("#processalert").show();
@@ -82,26 +104,57 @@ function addactivityTr(){
     }
     else{
         //後續會將活動編號(activity_1)改為dom
-        $("#activity_1Tbody").append('<tr>'+
-                                        '<th scope="row">'+num+'</th>'+
-                                        '<td>'+processtarget+'</td>'+
-                                        '<td>'+processcontent+'</td>'+
-                                        '<td>'+processtime+'</td>'+
-                                        '<td id="activity_1_assessmentTd_'+num+'">'+
-                                            '<a href="" class="assessment_link font-weight-bolder" class="assessment_link font-weight-bolder" data-toggle="modal" data-target="#addassessmentModal" data-targettr="activity_1_assessmentTd_'+num+'">新增評量方式...</a>'+
-                                        '</td>'+
-                                        '<td>'+processremark+'</td>'+
-                                        '<td>'+
-                                            '<input type="button" class="btn btn-warning mb-1" value="編輯">'+
-                                            '<input type="button" class="btn btn-danger btnDelete" value="刪除">'+
-                                        '</td>'+
-                                    '</tr>');
+        // $("#activity_1Tbody").append('<tr>'+
+        //                                 '<th scope="row">'+num+'</th>'+
+        //                                 '<td>'+processtarget+'</td>'+
+        //                                 '<td>'+processcontent+'</td>'+
+        //                                 '<td>'+processtime+'</td>'+
+        //                                 '<td id="activity_1_assessmentTd_'+num+'">'+
+        //                                     '<a href="" class="assessment_link font-weight-bolder" class="assessment_link font-weight-bolder" data-toggle="modal" data-target="#addassessmentModal" data-targettr="activity_1_assessmentTd_'+num+'">新增評量方式...</a>'+
+        //                                 '</td>'+
+        //                                 '<td>'+processremark+'</td>'+
+        //                                 '<td>'+
+        //                                     '<input type="button" class="btn btn-warning mb-1" value="編輯">'+
+        //                                     '<input type="button" class="btn btn-danger btnDelete" value="刪除">'+
+        //                                 '</td>'+
+        //                             '</tr>');
+        var rows = [];
+        
+        rows.push({
+            tr_num:num,
+            activity_process_learningtarget:processtarget,
+            activity_process_content:processcontent,
+            activity_process_time:processtime,
+            activity_process_assessment:'<a href="" class="assessment_link font-weight-bolder" class="assessment_link font-weight-bolder" data-toggle="modal" data-target="#addassessmentModal" data-targettr="activity_1_assessmentTd_'+num+'">新增評量方式...</a>',
+            activity_process_remark:processremark
+        })
+
         $("#addprocessModal").modal("hide");
-        $("#processcontent").summernote("reset");
+        $("#processcontent").summernote("code",'');
         $("#addprocessModal input[type='text']").val("");
         $("#addprocessModal input[type='number']").val("");
         $("#processalert").hide();
+
+        //刪除後append有問題，刪除改寫remove
+        $activitytable.bootstrapTable('append', rows);
+        console.log(rows)//只有一筆
     }  
+}
+
+function processBtnFormatter(){
+    return[
+        '<input type="button" class="btn btn-warning mb-1" value="編輯">',
+        '<input type="button" class="btn btn-danger btnDelete" value="刪除">'
+    ].join('');
+}
+
+window.processBtnEvents = {
+    'click .btnDelete': function (e, value, row, index) {
+      $activitytable.bootstrapTable('remove', {
+        field: 'tr_num',
+        values: [row.tr_num]
+      })
+    }//會把全部都刪掉
 }
 
 function addassessmentTd(){
@@ -111,11 +164,11 @@ function addassessmentTd(){
                             '<hr>'+
                             '<div class="assessment_content">'+assessmentcontent+'</div>'+
                             '<div class="btn-group">'+
-                            '<a href="" class="assessment_link"><small>編輯</small></a>'+
-                            '<a href="" class="assessment_link_del ml-1"><small>刪除</small></a>'+
+                                '<input type="button" class="btn btn-sm btn-link assessment_link" value="編輯">'+
+                                '<input type="button" class="btn btn-sm btn-link assessment_link_del" onclick="deleteassessment()" value="刪除">'+
                             '</div>'+
                         '</div>');
-    $("#assessmentsummernote").summernote("reset");
+    $("#assessmentsummernote").summernote("code",'');
     $("#addassessmentModal").modal("hide");
 }
 
@@ -123,17 +176,21 @@ function addassessmentTd(){
 function modalclosebtn(modalid){
     switch (modalid){
         case 'addprocessModal':
-            $("#processcontent").summernote("reset");
+            $("#processcontent").summernote("code",'');
             $("#addprocessModal input[type='text']").val("");
             $("#addprocessModal input[type='number']").val("");
             $("#processalert").hide();
             break;
         case 'addassessmentModal':
-            $("#assessmentsummernote").summernote("reset");
+            $("#assessmentsummernote").summernote("code",'');
             break;
     }
 }
 
 function resetsummernote(){
-    $("#assessmentsummernote").summernote("reset");   
+    $("#assessmentsummernote").summernote("code",'');  
+}
+
+function deleteassessment(){
+    $(this).find('.assessmentDiv').remove();
 }
